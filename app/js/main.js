559f2edb8f35d930c0e3940b293020e76ee63741
EgorVertaev <eholife123@gmail.com>
2022-08-12 21:33:47 +0300

    added preview section and reasons, footer and add response for headeer and start to dev menu-mob

--- app/js/main.js
+++ app/js/main.js
@@ -1,4 +1,27 @@
 
+const burger = document.querySelector('.menu__burger')
+const burgerInner = document.querySelector('.menu__burger-inner')
+const menuMob = document.querySelector('.menu-mob')
+const body = document.querySelector('body')
+const wrapper = document.querySelector('.wrapper')
+
+burger.addEventListener('click', (e) => {
+  e.stopPropagation();
+  burger.classList.toggle('menu__burger--active')
+  burgerInner.classList.toggle('menu__burger-inner--active')
+  body.classList.toggle('overlay')
+  menuMob.classList.toggle('menu-mob--active')
+})
+
+wrapper.addEventListener('click', () => {
+  body.classList.remove('overlay')
+  menuMob.classList.remove('menu-mob--active')
+  burger.classList.remove('menu__burger--active')
+  burgerInner.classList.remove('menu__burger-inner--active')
+})
+
+
+
 const swiper = new Swiper('.discond-slider', {
 
   navigation: {
